6fc9c9ac5d8c267190dbe3b813a1eeb0cc5ca49a
nriddiford <nickriddiford@me.com>
2021-01-05 14:46:51 +0100

    Change defaults

--- bin/write_files.py
+++ bin/write_files.py
@@ -55,10 +55,10 @@ def get_args():
 
     parser.add_option("--out_file", "-o", dest="out_file", action="store", help="File to write config to [Default 'my_sample_plan.csv']")
     parser.add_option("--config", "-c", dest="config", action="store", help="Mapping for tumour/normal samples [Default 'data/samples.tsv']")
-    parser.add_option("--directory", "-d", dest="dir", action="store", help="Directory to look for files in [Default cwd]")
+    parser.add_option("--directory", "-d", dest="dir", action="store", help="Directory to look for files in [Default data/]")
     parser.add_option("--extension", "-e", dest="ext", action="store", help="Extension to search for [Default 'fastq.gz']")
 
-    parser.set_defaults(dir=os.getcwd(), ext='fastq.gz', config='data/samples.tsv', out_file='my_sample_plan.csv')
+    parser.set_defaults(dir='data/', ext='fastq.gz', config='data/samples.tsv', out_file='my_sample_plan.csv')
     options, args = parser.parse_args()
 
     return options, args
