5e8887766d5f9232939c15aac747c16a853d9c70
nriddiford <nickriddiford@me.com>
2021-01-19 15:28:46 +0100

    Update

--- bin/write_files.py
+++ bin/write_files.py
@@ -1,9 +1,14 @@
 import fnmatch
 import os
 import sys
+import re
 from collections import defaultdict
 from optparse import OptionParser
 import pandas as pd
+import glob
+import json
+
+# import fnmatch
 
 
 def find_normal(options):
@@ -18,6 +23,22 @@ def find_normal(options):
 
     return s
 
+def get_reads(options, id, assay, d):
+    file_path = os.path.join(options.dir)
+    reads = glob.glob(file_path + id + '*')
+
+    if reads:
+        f_match = re.compile(".*(R1|forward).*" + options.ext)
+        r_match = re.compile(".*(R2|reverse).*" + options.ext)
+        # f_read = glob.glob(t_reads + '*R1*' + options.ext)
+        r1 = list(filter(f_match.match, reads))[0] # Read Note
+        r2 = list(filter(r_match.match, reads))[0] # Read Note
+
+        print("Assay: %s, R1: %s, R2: %s" % (assay, r1, r2))
+
+        # d.push({"tumour_id": id})
+
+        return d
 
 def write_plan(options):
     print("Looking for '*%s' files in %s" % (options.ext, options.dir))
@@ -27,22 +48,30 @@ def write_plan(options):
 
     file_mapping = [
         {
-            'tumour_id': {
-                'tr1': '',
-                'tr2': ''
-            },
-            'normal_id': {
-                'tr1': '',
-                'tr2': ''
-            }
+            'tumour_id': [],
+            'normal_id': []
         }
     ]
+            # 'tumour_id': [],
+            # 'normal_id': []
 
     for tumour_id in tumour_mapping.keys():
-        file_path = os.path.join(options.dir)
+
         # for file in glob.glob('data/HUM-1*'):
-        file_mapping[glob.glob('data/HUM-1*')] = glob.glob('data/HUM-3*')
-        file_mapping
+        # file_mapping[glob.glob('data/HUM-1*')] = glob.glob('data/HUM-3*')
+        # file_mapping[tumour_id] = glob.glob('data/HUM-1*')
+        # t_reads = glob.glob('data/' + tumour_id + '*')
+
+        file_mapping = get_reads(options, tumour_id, 'tumour', file_mapping)
+
+        # if t_reads:
+        #     r1 = re.compile(".*R1.*" + options.ext)
+        #     r2 = re.compile(".*R2.*" + options.ext)
+        #     # f_read = glob.glob(t_reads + '*R1*' + options.ext)
+        #     tr1 = list(filter(r1.match, t_reads))[0] # Read Note
+        #     tr2 = list(filter(r2.match, t_reads))[0] # Read Note
+        #
+        #     file_mapping[tumour_id] = [tr1, tr2]
 
     # with open(options.out_file, 'w') as out:
     #     out.write('tumour_id,tr1,tr2,normal_id,nr1,nr2\n')
@@ -65,6 +94,8 @@ def write_plan(options):
     #                 out.write(l + ',')
     #             else:
     #                 out.write(l + '\n')
+
+    print(json.dumps(file_mapping, indent=4, sort_keys=True))
 
     return True
 
